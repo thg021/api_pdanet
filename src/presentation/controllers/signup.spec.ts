32bcbd65f9255229897fefac917fed96a7760a9a
Thiago Silva <thiago.silva@hotmail.com>
2021-10-27 00:09:38 -0300

    test: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,18 @@
+import { SignUpController } from './signup';
+
+describe('Signup controller', () => {
+    test('Should return 400 if no name is provided', () => {
+        //system under test
+        const sut = new SignUpController();
+        const httpRequest = {
+            body: {
+                email: 'any_email@email.com',
+                password: 'any_password',
+                passwordConfirmation: 'any_password',
+            },
+        };
+
+        const httpResponse = sut.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+    });
+});
