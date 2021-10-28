6a84d2c75eef722849d5c8892319fdbdbee9abc9
Thiago Silva <thiago.silva@hotmail.com>
2021-10-28 06:02:06 -0300

    feat: ensure signupcontroller return 200 if valid data is provided

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -46,11 +46,16 @@ export class SignUpController implements IController {
                 );
             }
 
-            this.addAccount.add({
+            const account = this.addAccount.add({
                 name,
                 email,
                 password,
             });
+
+            return {
+                statusCode: 200,
+                body: account,
+            };
         } catch (error) {
             return serverError();
         }
