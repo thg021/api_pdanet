5c21f296a8da9aeb317c6d4846463568b6b8de42
Thiago Silva <thiago.silva@hotmail.com>
2021-11-01 16:31:05 -0300

    feat: ensure cors is enable

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express';
 import { bodyParser } from '../midlewares/body-parser';
+import { cors } from '../midlewares/cors';
 
 export default (app: Express): void => {
     app.use(bodyParser);
+    app.use(cors);
 };
