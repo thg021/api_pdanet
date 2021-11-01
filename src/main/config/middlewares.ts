47025e23af5152be0b7eb14072a71b774e9da5f6
Thiago Silva <thiago.silva@hotmail.com>
2021-11-01 17:43:53 -0300

    refactor: group middlaware interfaces in sigle file

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,5 @@
 import { Express } from 'express';
-import { bodyParser } from '../midlewares/body-parser';
-import { contentType } from '../midlewares/content-type';
-import { cors } from '../midlewares/cors';
+import { bodyParser, contentType, cors } from '../midlewares';
 
 export default (app: Express): void => {
     app.use(bodyParser);
