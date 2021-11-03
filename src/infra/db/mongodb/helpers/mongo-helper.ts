2308b5f48a9eab38d48aaa21a93ed8828b468c4e
Thiago Silva <thiago.silva@hotmail.com>
2021-11-03 16:25:14 -0300

    test: ensure mongodb reconnect is client is down

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -9,7 +9,7 @@ export const MongoHelper = {
     },
     async disconnect(): Promise<void> {
         await this.client.close();
-        'this.client = null;
+        this.client = null;
     },
     async getCollection(name: string): Promise<Collection> {
         if (!this.client?.on) {
